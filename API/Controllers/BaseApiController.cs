3a30d18b3afdc6be55ca7d45483da3234cbd8272
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-06 14:57:12 -0500

    Fetch to Api - adding semantic ui

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
         
