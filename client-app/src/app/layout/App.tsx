c1d0dde515f10318ff096a4441fad30a9422d6a6
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-10 18:05:49 -0500

    Creating form and activity list with semantic fmw

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+    .then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <>
+        <NavBar />
+        <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard activities={activities} />
+        </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Grid, List } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+import ActivityList from './ActivityList';
+
+interface Props {
+    activities: Activity[];
+}
+
+export default function ActivityDashboard({activities}: Props){
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities}/>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] && 
+                <ActivityDetails activity={activities[0]}/> }
+                <ActivityForm />
+            </Grid.Column>
+        </Grid>
+    )
+}
