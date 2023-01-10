a3f3ed59c0ddf6a75f61221e5d3cd306a45450cc
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-10 18:47:28 -0500

    performing to show and hide form to create or edit activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,8 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -15,11 +17,35 @@ function App() {
     })
   }, [])
 
+  function handleSelectedActivity(id: string){
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string){
+    id ? handleSelectedActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
   return (
     <>
-        <NavBar />
+        <NavBar openForm={handleFormOpen} />
         <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard activities={activities} />
+          <ActivityDashboard activities={activities} 
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectedActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            editMode={editMode}
+            openForm={handleFormOpen}
+            closeForm={handleFormClose}
+          />
         </Container>
     </>
   );
