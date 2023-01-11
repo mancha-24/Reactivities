29e0e1b3384a28f96fce0a34f19fb44c6bfe1aff
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-11 16:56:02 -0500

    using mobX for use of states

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,25 +6,20 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+  const {activityStore} = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [submiting, setSubmiting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
 
   function handleSelectedActivity(id: string){
     setSelectedActivity(activities.find(x => x.id === id));
@@ -72,26 +67,28 @@ function App() {
     
   }
 
-  if(loading) return <LoadingComponent content='Loading app' />
+  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <>
         <NavBar openForm={handleFormOpen} />
         <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard activities={activities} 
-            selectedActivity={selectedActivity}
-            selectActivity={handleSelectedActivity}
-            cancelSelectActivity={handleCancelSelectActivity}
-            editMode={editMode}
-            openForm={handleFormOpen}
-            closeForm={handleFormClose}
-            createOrEdit={handleCreateOrEditActivity}
-            deleteActivity={handleDeleteActivity}
-            submiting={submiting}
+
+          <ActivityDashboard 
+                              activities={activityStore.activities} 
+                              selectedActivity={selectedActivity}
+                              selectActivity={handleSelectedActivity}
+                              cancelSelectActivity={handleCancelSelectActivity}
+                              editMode={editMode}
+                              openForm={handleFormOpen}
+                              closeForm={handleFormClose}
+                              createOrEdit={handleCreateOrEditActivity}
+                              deleteActivity={handleDeleteActivity}
+                              submiting={submiting}
           />
         </Container>
     </>
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,12 +4,16 @@ import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
