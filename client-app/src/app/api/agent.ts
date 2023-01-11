d6fc9ef4ce7370f99647432f72e8840079ca90ca
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-11 14:59:11 -0500

    adding agent for axios request to api and loader

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from 'axios';
+import { url } from 'inspector';
+import { Activity } from '../models/activity';
+
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    })
+}
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
+
+const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+
+const request = {
+    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
+    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
+}
+
+const Activities = {
+    list: () => request.get<Activity[]>('/activities')
+}
+
+const agent = {
+    Activities
+}
+
+export default agent;
