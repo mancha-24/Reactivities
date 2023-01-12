e9f844ddc952a0fd8ce79b97bed48095b1742d74
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-12 15:28:24 -0500

    adding react router: npm install react-router-dom

---
+++ client-app/src/app/router/Routes.tsx
@@ -0,0 +1,12 @@
+import { RouteObject } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../layout/App";
+
+export const routes: RouteObject[] = [
+    {
+        path: '/',
+        element: <App />
+    }
+]
+
+export const router = createBrowserRouter(routes);
