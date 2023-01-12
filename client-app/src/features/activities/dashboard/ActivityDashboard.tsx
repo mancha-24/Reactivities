df8d6c4e46bf49b68f228981cd7b3a2a26241eb1
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-12 16:44:06 -0500

    using routing to navigate around the pages and some fixes

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,26 +1,27 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
 export default observer( function ActivityDashboard(){
-
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
+    const {loadActivities, activityRegistry} = activityStore;
+
+    useEffect(() => {
+     if(activityRegistry.size <= 1) activityStore.loadActivities();
+    }, [loadActivities])
+  
+    if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails /> }
-                {editMode &&
-                <ActivityForm />}
+                <h2>Activity Filters</h2>
             </Grid.Column>
         </Grid>
     )
