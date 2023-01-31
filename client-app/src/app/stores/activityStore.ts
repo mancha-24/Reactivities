299e0d395ad34778e3e1a401732d103605243605
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-31 09:10:05 -0500

    Add activity list item by date

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 import {v4 as uuid} from 'uuid';
+import { act } from "@testing-library/react";
 
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
@@ -16,6 +17,16 @@ export default class ActivityStore {
 
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) -Date.parse(b.date));
+    }
+
+    get gruopedActivities () {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: Activity[]})
+        )
     }
 
     loadActivities = async () => {
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,48 +1,32 @@
 import { observer } from "mobx-react-lite";
-import React, { SyntheticEvent, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Fragment } from "react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import ActivityListItem from "./ActivityListItem";
 
 export default observer(function ActivityList() {
     const {activityStore} = useStore();
-    const {deleteActivity, activitiesByDate, loading} = activityStore;
-
-    const[target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
+    const {gruopedActivities} = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divaded>
-                {
-                    activitiesByDate.map(activity => (
-                        <Item key={activity.id}>
-                            <Item.Content>
-                                <Item.Header as='a'>{activity.title}</Item.Header>
-                                <Item.Meta>{activity.date}</Item.Meta>
-                                <Item.Description>
-                                    <div>{activity.description}</div>
-                                    <div>{activity.city} {activity.venue}</div>
-                                </Item.Description>
-                                <Item.Extra>
-                                    <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='view' color='blue' />
-                                    <Button loading={loading && target === activity.id}
-                                            name={activity.id} 
-                                            onClick={(e) => handleActivityDelete(e, activity.id)} 
-                                            floated='right' 
-                                            content='Delete' 
-                                            color='red' />
-                                    <Label basic content={activity.category} />
-                                </Item.Extra>
-                            </Item.Content>
-                        </Item>
-                    ))
-                }
-            </Item.Group>
-        </Segment>
+        <>
+            {gruopedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color='teal'>
+                        {group}
+                    </Header>
+                    <Segment>
+                        <Item.Group divaded>
+                            {
+                                activities.map(activity => (
+                                    <ActivityListItem key={activity.id} activity={activity}/>
+                                ))
+                            }
+                        </Item.Group>
+                    </Segment>
+                </Fragment>
+            ))}
+        </>
+        
     )
 })
