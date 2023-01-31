88324dd6644d9dd71472ed62675d8697fe9820a4
Manuel Chavez <manuel1995_01@outlook.com>
2023-01-31 15:28:38 -0500

    adding test errors and buggy controller for testing

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import TestErrors from "../../features/errors/TestError";
 import App from "../layout/App";
 
 export const routes: RouteObject[] = [
@@ -14,6 +15,7 @@ export const routes: RouteObject[] = [
             { path: 'activities/:id', element:<ActivityDetails /> },
             { path: 'createActivity', element:<ActivityForm key='create' /> },
             { path: 'manage/:id', element:<ActivityForm key='manage' /> },
+            { path: 'errors', element:<TestErrors /> },
         ]
     }
 ]
