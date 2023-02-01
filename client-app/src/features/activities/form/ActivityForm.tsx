b1a1d7cb68ed54032707d3f50f1535f30e096f2f
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-01 10:34:07 -0500

    creating a custom text input with hook useField

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import {v4 as uuid} from 'uuid';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
@@ -28,7 +29,12 @@ export default observer(function ActivityForm() {
     });
 
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required')
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        venue: Yup.string().required(),
+        city: Yup.string().required()
     })
 
     useEffect(() => {
@@ -61,17 +67,12 @@ export default observer(function ActivityForm() {
                 {({ handleSubmit }) => {
                     return (
                         <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                            <FormField>
-                                <Field placeholder='Title' name='title' />
-                                <ErrorMessage name='title'
-                                    render={error => <Label basic color='red' content={error} />} />
-                            </FormField>
-                            <Field placeholder='Title' name='title' />
-                            <Field placeholder='Description' name='description' />
-                            <Field placeholder='Category' name='category' />
-                            <Field type='date' placeholder='Date' name='date' />
-                            <Field placeholder='City' name='city' />
-                            <Field placeholder='Venue' name='venue' />
+                            <MyTextInput placeholder='title' name='title' />
+                            <MyTextInput placeholder='Description' name='description' />
+                            <MyTextInput placeholder='Category' name='category' />
+                            <MyTextInput placeholder='Date' name='date' />
+                            <MyTextInput placeholder='City' name='city' />
+                            <MyTextInput placeholder='Venue' name='venue' />
                             <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                             <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                         </Form>
