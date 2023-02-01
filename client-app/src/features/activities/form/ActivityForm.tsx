81b85a1bdf9e68fa79c0f9fdfbe6f344bf9ff314
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-01 11:00:37 -0500

    creating custom select input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,6 +10,8 @@ import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
+import MySelectInput from '../../../app/common/form/MySelectInput';
+import { categoryOptions } from '../../../app/common/options/categoryOptions';
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
@@ -70,7 +72,7 @@ export default observer(function ActivityForm() {
                         <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                             <MyTextInput placeholder='title' name='title' />
                             <MyTextArea rows={3} placeholder='Description' name='description' />
-                            <MyTextInput placeholder='Category' name='category' />
+                            <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
                             <MyTextInput placeholder='Date' name='date' />
                             <MyTextInput placeholder='City' name='city' />
                             <MyTextInput placeholder='Venue' name='venue' />
