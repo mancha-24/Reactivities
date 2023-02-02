f79217d88a94c41471b423ed630916cc07566af2
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-02 15:38:57 -0500

    eagly load relations and adding automapper settings

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,5 @@
 using Application.Core;
-using Domain;
+using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -8,20 +8,29 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                    _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                var activities = await _context.Activities
+                                .Include(a => a.Attendees)
+                                .ThenInclude(u => u.AppUser)
+                                .ToListAsync(cancellationToken);
+
+                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+
+                return Result<List<ActivityDto>>.Success(activitiesToReturn);
             }
         }
     }
