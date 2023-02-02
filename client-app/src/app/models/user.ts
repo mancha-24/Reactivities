492d60918044ed48a15fffb7d90a0cd763e1d990
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-02 11:51:48 -0500

    add user info in navbar and homepage

--- client-app/src/app/models/user.ts
+++ client-app/src/app/models/user.ts
@@ -1,6 +1,6 @@
 export interface User {
     username: string;
-    displayname: string;
+    displayName: string;
     token: string;
     image?: string;
 }
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,10 @@
+import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const {userStore} = useStore();
     return (
         <Segment inverted textAlign="center" vertical className="masthead">
             <Container text>
@@ -9,11 +12,20 @@ export default function HomePage() {
                     <Image size="massive" src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
                     Reactivities
                 </Header>
-                <Header as='h2' inverted content='Welcome to Reactivities' />
-                <Button as={Link} to='/login' size="huge" inverted>
-                    Login!
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Reactivities' />
+                        <Button as={Link} to='/activities' size="huge" inverted>
+                            Go to Activities!
+                        </Button>
+                    </>
+                ) : (
+                        <Button as={Link} to='/login' size="huge" inverted>
+                            Login!
+                        </Button>
+                )}
+                
             </Container>
         </Segment>
     )
-}
+})
