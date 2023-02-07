9212c4f736d224eb87f57b49eac6fc141ba4bed3
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-07 10:04:22 -0500

    setting up SignalR on API

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -39,6 +39,7 @@ namespace API.Extensions
                 services.AddScoped<IUserAccessor, UserAccessor>();
                 services.AddScoped<IPhotoAccessor, PhotoAccessor>();
                 services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+                services.AddSignalR();
 
                 return services;
         }
