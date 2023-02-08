919552430931683f5ae43299a6282bee9c39aa4c
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-08 12:02:17 -0500

    adding axios param to pagination ClientApp

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -76,7 +76,8 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<PaginatedResult<Activity[]>>('/activities'),
+    list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', {params})
+        .then(responseBody),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post('/activities', activity),
     update: (activity: ActivityFormValues) => requests.put(`/activities/${activity.id}`, activity),
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,7 +5,7 @@ import {v4 as uuid} from 'uuid';
 import {format} from 'date-fns';
 import { store } from "./store";
 import { Profile } from "../models/profile";
-import { Pagination } from "../models/pagination";
+import { Pagination, PagingParams } from "../models/pagination";
 
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
@@ -14,9 +14,21 @@ export default class ActivityStore {
     loading = false;
     loadingInitial = false;
     pagination: Pagination | null = null;
+    pagingParams= new PagingParams();
 
     constructor() {
         makeAutoObservable(this)
+    }
+
+    setPagingParams = (pagingParams: PagingParams) => {
+        this.pagingParams = pagingParams;
+    }
+
+    get axiosParams() {
+        const params = new URLSearchParams();
+        params.append('pageNumber', this.pagingParams.pageNumber.toString());
+        params.append('pageSize', this.pagingParams.pageSize.toString());
+        return params;
     }
 
     get activitiesByDate() {
@@ -36,7 +48,7 @@ export default class ActivityStore {
     loadActivities = async () => {
         this.setLoadingInitial(true);
         try {
-            const result = await agent.Activities.list();
+            const result = await agent.Activities.list(this.axiosParams);
                 result.data.forEach(activity => {
                     this.setActivity(activity);
                   })
