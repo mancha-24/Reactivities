50962c2e404af62b734aada945656a3777b9df2a
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-28 15:49:44 -0500

    set tag variable script

---
+++ setTagVariable.ps1
@@ -0,0 +1,12 @@
+$VariableGroupId = $(variable.group.id)
+$NewValue = "prueba"
+$VariableName = "reactivitiesContainerTag"
+Write-Host "NewValue : $NewValue"
+$url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/distributedtask/variablegroups/$($VariableGroupId)?api-version=5.1-preview.1"
+Write-Host "URL: $url"
+$authHeader = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
+$definition = Invoke-RestMethod -Uri $url -Headers $authHeader
+Write-Host "Pipeline = $($definition | ConvertTo-Json -Depth 100)"
+$definition.variables.$VariableName.Value = "$($NewValue)"
+$definitionJson = $definition | ConvertTo-Json -Depth 100 -Compress
+Invoke-RestMethod -Method Put -Uri $url -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
