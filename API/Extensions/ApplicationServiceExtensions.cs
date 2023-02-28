ff845412a35d05c7bda009f01a884d0afd4b5a21
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-28 12:19:06 -0500

    burning connection string for testing

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -21,7 +21,9 @@ namespace API.Extensions
                 services.AddSwaggerGen();
                 services.AddDbContext<DataContext>(opt =>
                 {
-                    opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+                    // opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+                    opt.UseNpgsql(config.GetConnectionString("Server=pgltitanium.postgres.database.azure.com; Port=5432; User Id=titan@pgltitanium; Password=$3cr3tPassword; Database=reactivities"));
+                    
                 });
 
                 services.AddCors(opt =>
